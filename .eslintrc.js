8d7bdd56f3c60597d36aa044a2c3d59746c90bdc
Aapo Attila <aapo.attila@windowslive.com>
2020-04-30 21:55:10 +0300

    Dependencies, eslint setup

---
+++ .eslintrc.js
@@ -0,0 +1,41 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true
+  },
+  extends: [
+    'plugin:react/recommended',
+    'airbnb',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBufferer: 'readonly',
+    test: 'readonly',
+    expect: 'readonly',
+  },
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: [
+    'react',
+  ],
+  rules: {
+    'arrow-parens': [1, 'as-needed'],
+    'camelcase': [1, { ignoreDestructuring: true, allow: [] }],
+    'no-console': 0,
+    'object-curly-newline': 0,
+    'no-confusing-arrow': 0,
+    'no-nested-ternary': 0,
+    'no-param-reassign': [1, { ignorePropertyModificationsFor: [] }],
+    'no-unused-vars': [1, { argsIgnorePattern: '^_'}],
+    'import/prefer-default-export': 0,
+    'react/forbid-prop-types': 0,
+    'react/jsx-props-no-spreading': [1, { exceptions: ['Fragment', 'Component'] }],
+    'react/state-in-constructor': 0,
+  }
+}
