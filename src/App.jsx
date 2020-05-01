37554a238cafe9243f1cadacfff37787dca71434
Aapo Attila <aapo.attila@windowslive.com>
2020-05-01 17:43:38 +0300

    background, logo, uninstall node-sass

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
 import React from 'react';
 import { withTranslation } from 'react-i18next';
 
-import NavBar from './components/Navbar';
-
-import './styles/app.scss';
+import logo from './assets/Logo.svg';
+import styles from './styles/app.module.css';
 
 const App = () => (
-  <span className="app-header">
-    <NavBar />
-  </span>
+  <div className={styles.app}>
+    <span className={styles.logo}>
+      <img alt="logo" src={logo} height={100} />
+    </span>
+  </div>
 );
 
 export default withTranslation()(App);
