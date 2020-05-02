dcc58a3a73caf0bcf4f5f35de3e5531079458037
Aapo Attila <aapo.attila@windowslive.com>
2020-05-02 20:13:53 +0300

    Initial button styling

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,22 @@
 import React from 'react';
 import { Trans, useTranslation } from 'react-i18next';
 import { Button } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
 import { Constants } from '../utils/constants';
 import { isFinnish } from '../utils/utils';
 import styles from '../styles/app.module.css';
 
+const useStyles = makeStyles(({
+  change: {
+    color: '#EA6227',
+    borderColor: '#EA6227',
+  },
+}));
+
 const Footer = () => {
   const { i18n } = useTranslation();
+
+  const classes = useStyles();
 
   const changeLanguage = () => {
     const { languages: { en, fi } } = Constants;
@@ -22,6 +32,7 @@ const Footer = () => {
       <span className={styles.changeLanguage}>
         <Button
           variant="outlined"
+          className={classes.change}
           onClick={changeLanguage}
         >
           <Trans i18nKey="language" />
