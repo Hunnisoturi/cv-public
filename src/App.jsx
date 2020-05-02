8eddcca0ac1937d556c269623fb61465196eb920
Aapo Attila <aapo.attila@windowslive.com>
2020-05-02 16:20:28 +0300

    Visual elements, initial footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,26 @@
 import React from 'react';
-import { withTranslation } from 'react-i18next';
+import { Trans, withTranslation } from 'react-i18next';
 import NavBar from './components/Navbar';
 
 import logo from './assets/Logo.svg';
+import rectangleLarge from './assets/RectangleLarge.svg';
 import styles from './styles/app.module.css';
 
 const App = () => (
   <div className={styles.app}>
+    <span className={styles.rectangle}>
+      <img alt="" src={rectangleLarge} />
+    </span>
     <span className={styles.appHeader}>
       <span className={styles.logo}>
         <img alt="logo" src={logo} height={110} />
       </span>
       <NavBar />
+    </span>
+    <span className={styles.appFooter}>
+      <footer>
+        <Trans i18nKey="copyright" />
+      </footer>
     </span>
   </div>
 );
