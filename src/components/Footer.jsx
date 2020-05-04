4f1f46448dc9843b34b318dd16365257ddbc8ad6
Aapo Attila <aapo.attila@windowslive.com>
2020-05-04 15:14:19 +0300

    added language switching back

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
-import { Trans } from 'react-i18next';
-import { Container, Typography } from '@material-ui/core';
+import { Trans, useTranslation } from 'react-i18next';
+import { Container, Typography, Button } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
+import { Constants } from '../utils/constants';
+import { isFinnish } from '../utils/utils';
 
 const styles = makeStyles({
   footer: {
@@ -11,19 +13,33 @@ const styles = makeStyles({
     bottom: '0',
     position: 'fixed',
     width: '100%',
-    height: '4rem',
-    alignItems: 'center',
+    height: '5rem',
+    justifyContent: 'space-between',
   },
 });
 
 const Footer = () => {
   const classes = styles();
+  const { i18n } = useTranslation();
+
+  const changeLanguage = () => {
+    const { languages: { en, fi } } = Constants;
+
+    i18n.changeLanguage(isFinnish(i18n.language) ? en : fi);
+  };
+
   return (
     <footer className={classes.footer}>
       <Container maxWidth="xl">
         <Typography variant="h6" align="center">
           <Trans i18nKey="copyright" />
         </Typography>
+        <Button
+          variant="outlined"
+          onClick={changeLanguage}
+        >
+          <Trans i18nKey="language" />
+        </Button>
       </Container>
     </footer>
   );
