3484fe8b4b0114d6ddbd88e7dd2482f3b67e4f38
Aapo Attila <aapo.attila@windowslive.com>
2020-05-08 14:46:43 +0300

    frontend .env

--- front/src/services/danxios.js
+++ front/src/services/danxios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const backendUrl = 'http://localhost:3002';
+const backendUrl = process.env.REACT_APP_BACKEND_URL;
 
 export default axios.create({
   baseURL: backendUrl,
